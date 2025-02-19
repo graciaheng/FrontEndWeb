11d074d43c488c685fd7d247010c221a912488e3
unknown <you@g00427320@atu.ie>
2025-02-19 11:12:18 +0000

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//import the new components
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'root-directive',
+  imports: [RouterOutlet, AComponent, BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'my-first-app';
+  //declare variables for databinding
+  name: string = "Gracia";
+  age: number = 20;
 }
