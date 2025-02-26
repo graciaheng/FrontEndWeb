9d3981a4530af05c3791d7682fe20866e452a069
unknown <you@g00427320@atu.ie>
2025-02-26 11:28:00 +0000

    Lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'data-binding-app';
+
+  //declare variables
+  count: number = 0;
+  show: boolean = false;
+
+  //function occurs when button is clicked
+  onButtonClick(){
+    console.log('Button Clicked');
+    this.count++;
+  }
+
+  onStarDBLClick(){
+    //if DBL click again, the text should not appear
+    if (this.show == true){
+      this.show = false;
+    }
+    //else, text should appear
+    else if (this.show == false){
+      this.show = true;
+    }
+  }
 }
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; //to use ngfor
+
+@Component({
+  selector: 'app-list',
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  //declare variables
+  presidents: string[] = ["Higgins", "McAleese", "Robinson", "Hillery"];
+
+  presidents2: any[] =
+  [{name: "Higgins", term:"2011-"},
+   {name: "McAleese", term:"1997-2011"},
+   {name: "Robinson", term:"1990-1997"},
+   {name: "Hillery", term:"1976-1990"},
+   {name: "O Dalaigh", term:"1974-1976"}];
+
+}
